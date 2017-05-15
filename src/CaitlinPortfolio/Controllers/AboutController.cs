4c74d74a027e03d67d31906c6128de482d101740
Caitlin Hines <caitlinhines@me.com>
2017-05-15 13:13:06 -0700

    changed styling on index

--- src/CaitlinPortfolio/Controllers/AboutController.cs
+++ src/CaitlinPortfolio/Controllers/AboutController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CaitlinPortfolio.Controllers
